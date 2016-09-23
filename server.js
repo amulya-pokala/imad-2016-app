99c95023d35448e2118becb6686e1d26dd711dd6
amulya-pokala <amulya22pokala@gmail.com>
2016-09-23 16:17:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(createTemplate(articleOne));
+  res.send(createTemplate(articleOne));
 
 });
 app.get('/article-two', function (req, res) {
