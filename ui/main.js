58a287568b2fc311ab4f6bd8c7727ebd63652574
amulya-pokala <amulya22pokala@gmail.com>
2016-09-25 13:12:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
 console.log('Loaded!');
+
+var element=document.getElementById("main-text");
+
+element.innerHTML="NEW VALUE"
+//move the image
+var marginLeft=0;
+var img=document.getElementById('madi');
+function moveRight() {
+    marginLeft=marginLeft+1;
+    img.style.marginLeft=marginLeft+'px';
+}
+img.onclick=function()
+{
+    var interval=setInterval(moveRight,50);
+}
